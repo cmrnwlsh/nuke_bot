75e794e951287ba54f17fbeb0f905cd06f3c8cd7
cameron <cmrnwlsh@gmail.com>
2023-08-22 04:05:19 -0400

    added requirements.txt

--- bot.py
+++ bot.py
@@ -38,7 +38,6 @@ class Confirm(View):
 
     @button(label='Cancel', style=ButtonStyle.red)
     async def cancel(self, interaction, button):
-        await interaction.delete_original_response()
         self.stop()
 
 
